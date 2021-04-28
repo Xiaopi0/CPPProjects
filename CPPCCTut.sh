719d7cf7a4f246e0d8a6c1c10b578e6de121195d
ShadePi <erikkirkegaardstage@gmail.com>
2021-04-28 16:01:06 +0200

    assembly

--- CPPCCTut.sh
+++ CPPCCTut.sh
@@ -1,3 +1,12 @@
 cd C++CCTut
-g++ Main.cpp -o Main.out
-./Main.out
+read -p 'Do you want to compile to [E]xecutable or [A]ssembly: ' EA
+if [ $EA == 'E' ]
+then
+ g++ Main.cpp -o Main.out
+ ./Main.out
+elif [ $EA == 'A' ]
+then
+ g++ -S Main.cpp
+else
+ echo "Invalid Option"
+fi
